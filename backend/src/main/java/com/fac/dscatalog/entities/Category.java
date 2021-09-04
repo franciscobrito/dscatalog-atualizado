2c29f6615a7bd38dcac4e2b4e9a00260683d7775
Francisco Brito <francisco.brito.ac@gmail.com>
2021-09-04 14:40:32 -0300

    Categories controller

---
+++ backend/src/main/java/com/fac/dscatalog/entities/Category.java
@@ -0,0 +1,23 @@
+package com.fac.dscatalog.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Category implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	
+}
